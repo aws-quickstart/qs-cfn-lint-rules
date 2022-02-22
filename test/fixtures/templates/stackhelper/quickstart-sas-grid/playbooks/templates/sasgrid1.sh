fdf1ae99eb0e5d105f6fe92b01094d5f1e6ca6a3
Troy Lindsay <tlindsay42@users.noreply.github.com>
2022-02-22 11:47:03 -0600

    Apply trailing-whitespace recommendations

--- test/fixtures/templates/stackhelper/quickstart-sas-grid/playbooks/templates/sasgrid1.sh
+++ test/fixtures/templates/stackhelper/quickstart-sas-grid/playbooks/templates/sasgrid1.sh
@@ -15,28 +15,28 @@ exit
 EOF
 
 sudo -u sasinst bash << EOF
-cd /sas/$1                       
-./setup.sh -deploy -quiet -responsefile /sas/quickstart/playbooks/templates/grid1_install.txt 
+cd /sas/$1
+./setup.sh -deploy -quiet -responsefile /sas/quickstart/playbooks/templates/grid1_install.txt
 exit
 EOF
 
 sudo -u root bash << EOF
-/sas/SASHome/SASFoundation/9.4/utilities/bin/setuid.sh 
+/sas/SASHome/SASFoundation/9.4/utilities/bin/setuid.sh
 exit
 EOF
 
 sudo -u sasinst bash << EOF
-cd /sas/$1                        
+cd /sas/$1
 . /sas/lsf/conf/profile.lsf
-./setup.sh -deploy -quiet -responsefile /sas/quickstart/playbooks/templates/grid1_config.txt 
-./setup.sh -deploy -quiet -responsefile /sas/quickstart/playbooks/templates/studio_config.txt 
+./setup.sh -deploy -quiet -responsefile /sas/quickstart/playbooks/templates/grid1_config.txt
+./setup.sh -deploy -quiet -responsefile /sas/quickstart/playbooks/templates/studio_config.txt
 exit
 EOF
 
 sudo -u root bash << EOF
 echo '-WORK /saswork' >> /sas/SASHome/SASFoundation/9.4/nls/en/sasv9.cfg
 . /sas/lsf/conf/profile.lsf
-/sas/SASHome/SASFoundation/9.4/utilities/bin/setuid.sh 
+/sas/SASHome/SASFoundation/9.4/utilities/bin/setuid.sh
 . /sas/studioconfig/sasstudio.sh start
 exit
 EOF
--- test/fixtures/templates/stackhelper/quickstart-sas-grid/playbooks/templates/sasgrid_all.sh
+++ test/fixtures/templates/stackhelper/quickstart-sas-grid/playbooks/templates/sasgrid_all.sh
@@ -6,17 +6,17 @@ echo "$HOSTNAME"
 
 sudo -u root bash << EOF
 if ! grep -q "@sas" /etc/security/limits.conf; then
-    echo "@sas             hard    nofile         20480" >> /etc/security/limits.conf 
-    echo "@sas             soft    nofile         20480" >> /etc/security/limits.conf 
-    echo "@sas             hard    nproc          20480" >> /etc/security/limits.conf 
-    echo "@sas             soft    nproc          20480" >> /etc/security/limits.conf 
+    echo "@sas             hard    nofile         20480" >> /etc/security/limits.conf
+    echo "@sas             soft    nofile         20480" >> /etc/security/limits.conf
+    echo "@sas             hard    nproc          20480" >> /etc/security/limits.conf
+    echo "@sas             soft    nproc          20480" >> /etc/security/limits.conf
 fi
 
-if ! grep -q "SASFoundation" /etc/profile; then  
+if ! grep -q "SASFoundation" /etc/profile; then
     echo 'export PATH=$PATH:/sas/SASHome/SASFoundation/9.4' >> /etc/profile
 fi
 
-if ! grep -q "profile.lsf" /etc/profile; then  
+if ! grep -q "profile.lsf" /etc/profile; then
     echo ". /sas/lsf/conf/profile.lsf" >> /etc/profile
 fi
 
