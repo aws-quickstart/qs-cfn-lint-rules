bacc33453dd397b5e4b14d0b51813d337f4288be
smshete123 <smshete@amazon.com>
2023-06-29 14:22:55 -0700

    Added rule for checking if user is not assigned a group, and minor fix for the inline policy rule

--- qs_cfn_lint_rules/IAMUserNotInGroup.py
+++ qs_cfn_lint_rules/IAMUserNotInGroup.py
@@ -6,16 +6,16 @@ from cfnlint.rules import CloudFormationLintRule
 from cfnlint.rules import RuleMatch
 
 
-class IAMNoInlinePolicy(CloudFormationLintRule):
-    id = 'EIAMNoInlinePolicy' # New Rule ID
-    shortdesc = 'No inline IAM policies for a user' # A short description about the rule
-    description = 'IAM user should not have any inline policies. Should be centralized Policy object on group' # (Longer) description about the rule
+class IAMPolicyOnlyGroup(CloudFormationLintRule):
+    id = 'EIAMUserNotInGroup' # New Rule ID
+    shortdesc = 'User not assigned to group' # A short description about the rule
+    description = 'IAM User is not assigned to any group' # (Longer) description about the rule
     source_url = 'https://github.com/aws-quickstart/qs-cfn-lint-rules/tree/main' # A url to the source of the rule, e.g. documentation, AWS Blog posts etc
     tags = ["iam"] 
 
     def match(self, cfn):
         
-        """Check if user has inline IAM policies"""
+        """Check if user is mentioned in IAM policy"""
 
         resources = cfn.get_resources(['AWS::IAM::User'])
         matches = []
@@ -23,9 +23,10 @@ class IAMNoInlinePolicy(CloudFormationLintRule):
             path = ['Resources', resource_name, 'Properties']
 
             properties = resource.get('Properties')
-            if ((properties) and (properties.get('Policies'))):
-                if (len(properties.get('Policies')) >= 1):
-                    path = ['Resources', resource_name, 'Properties', 'Policies']
-                    message = 'IAM Users should not have inline policies'
+            if properties:
+                groups = properties.get('Groups')
+                if ((groups == None) or (groups[0] == None)):
+                    path = ['Resources', resource_name, 'Properties', 'Groups']
+                    message = 'An IAM User must be assigned to a group'
                     matches.append(RuleMatch(path, message.format(resource_name)))
         return matches
